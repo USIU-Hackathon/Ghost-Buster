88dacc45784261d0c7decbc549c7ed67162e16f4
Steve Waweru <steviewez@gmail.com>
2015-03-14 16:27:17 +0300

    api for doctors model

--- src/GhostBuster/urls.py
+++ src/GhostBuster/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+from doctors.views import DoctorViewSet
+
+router = DefaultRouter()
+router.register(r'doctor', DoctorViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'GhostBuster.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include(router.urls)),
 )
